f0d0287deaddce9e2468e67f6b36cb0f49b06421
Tim Kerkhoven <t.kerkhoven@student.utwente.nl>
2019-03-03 12:39:58 +0100

    fix version

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 love.conf = function(t)
     t.title = "Fluffy the Porcupine Saves the World"
-    t.version = "0.10.2"
+    t.version = "11.2"
     t.window.width = 1920
     t.window.height = 1080
     t.window.resizable = false
